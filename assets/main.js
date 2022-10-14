4c51cff66ad13a4614d323760c9806fb577a4217
Juan Carlos Damian Odar <judamian@minedu.gob.pe>
2022-10-14 11:12:56 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -29,14 +29,16 @@ async function fetchData(urlApi) {
         let view = `
         ${videos.items.map(video => `
             <div class="group relative">
-                <div
+                <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank"><div
                     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
                     <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-                </div>
+                </div></a>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
+                    <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
                     <span aria-hidden="true" class="absolute inset-0"></span>
                     ${video.snippet.title}
+                    </a>
                     </h3>
                 </div>
             </div>
